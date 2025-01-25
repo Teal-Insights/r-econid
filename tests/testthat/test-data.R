d45413781c0efa36119217f8573e645d75d0098f
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2025-01-25 15:30:53 -0500

    Removed tests aassociated with deleted data objects

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,20 +1,3 @@
-test_that("country_aggregates dataset has correct columns and no NAs", {
-  # Check if the dataset has the required columns
-  required_columns <- c("country_name", "country_group", "group_type")
-  expect_true(all(required_columns %in% colnames(country_aggregates)))
-
-  # Check if there are no NAs in the dataset
-  expect_true(all(complete.cases(country_aggregates)))
-})
-
-test_that("country_codes dataset has correct columns and no NAs", {
-  # Check if the dataset has the required columns
-  required_columns <- c("country", "is_wb_country", "wb_name", "cldr_short_en",
-                        "iso2c", "iso3c", "iso3n", "imf", "continent",
-                        "country_name_en_regex")
-  expect_true(all(required_columns %in% colnames(country_codes)))
-})
-
 test_that(
   "economy_patterns regex matches historical country names (all cases)", {
     # Join test_cases and economy_patterns
@@ -26,7 +9,7 @@ test_that(
 
     # Iterate over each row in test_cases
     purrr::pwalk(
-      test_cases,
+      test_cases[1:100, ],
       function(economy_name, economy_regex, iso3c, iso2c, variant_names) {
         # Error if any required field is NA or NULL
         if (any(is.na(c(
